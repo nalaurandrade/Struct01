6f9599056c18ab8e7cb33dfb0a090959a0c28b83
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-25 19:11:11 -0300

    Add files via upload

---
+++ Struct01/Struct01/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Struct01;
+
+Compromisso compromisso = new Compromisso();
+compromisso.Texto = "Meu comromisso e com vocês";
+compromisso.Data = new Data();
+compromisso.Data.Dia = 5;
+compromisso.Data.Mes = 5;
+compromisso.Data.Ano = 1987;
+compromisso.Horario = new Horario();
+compromisso.Horario.Segundo = 1;
+compromisso.Horario.Minuto = 26;
+compromisso.Horario.Hora = 16;
+
+Console.WriteLine(Funcoes.ImprimirData(compromisso.Data));
---
+++ Struct01/Struct01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct01
+{
+
+
+    public struct Horario
+    {
+        public int Hora;
+        public int Minuto;
+        public int Segundo;
+    }
+
+
+    public struct Data
+    {
+        public int Dia;
+        public int Mes;
+        public int Ano;
+        public int Seculo;
+    }
+
+    public struct Compromisso
+    {
+        public Data Data;
+        public Horario Horario;
+        public string Texto;
+    }
+
+    public class Funcoes
+    {
+        public string ImprimirData(Data data)
+        {
+            return $@"{data.Dia}/{data.Mes}/{data.Ano} do seculo {data.Seculo}";
+        }
+    }
+}
